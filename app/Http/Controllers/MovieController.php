66a07081b783a4a88af76ce7b9935ebecfd0d154
ClauGarfiasMx <patriciaga@protonail.com>
2019-09-09 19:45:32 -0500

    stucked with store request

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -14,51 +14,72 @@ class MovieController extends Controller
      */
     public function index()
     {
-        //
+        echo "hello";
+       
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function create()
+    // {
+        
+    // }
 
     /**
      * Store a newly created resource in storage.
+     * Debe crear un nuevo elemento con los parámetros recibidos.
+     * Es lo mismo que POST
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
-    }
+    //    echo "Hi from store";
+        print_r($request->all());
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Movie  $movie
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Movie $movie)
-    {
-        //
-    }
+        $movie = new Movie();
+        $movie->title = $request->input( key:'title');
+        $movie->description = $request->input( key:'description');
+        $movie->genre = $request->input( key:'genre');
+        $movie->year = $request->input( key:'year');
+        $movie->duration = $request->input( key:'duration');
+        $movie->save();
+        echo json_encode($movie);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Movie  $movie
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Movie $movie)
-    {
-        //
+
+
+
     }
+    
+
+    // /**
+    //  * Display the specified resource.
+    //  * Muestra los registros tal cual en una página,
+    //  * no interesa por ahora porque index hace lo mismo pero
+    //  * en formato JSON.
+    //  *
+    //  * @param  \App\Movie  $movie
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function show(Movie $movie)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  *
+    //  * @param  \App\Movie  $movie
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function edit(Movie $movie)
+    // {
+    //     //
+    // }
 
     /**
      * Update the specified resource in storage.
@@ -69,7 +90,7 @@ class MovieController extends Controller
      */
     public function update(Request $request, Movie $movie)
     {
-        //
+        echo "Hi from update";
     }
 
     /**
